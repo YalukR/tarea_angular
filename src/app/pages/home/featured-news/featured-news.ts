f4d56fd5368d6b25e7ff2aafe1bac7720fb98e2d
YalukR <yalukramos@gmail.com>
2026-05-29 12:56:02 -0600

    featured-news

--- src/app/pages/home/featured-news/featured-news.ts
+++ src/app/pages/home/featured-news/featured-news.ts
@@ -8,10 +8,20 @@ import { FeaturedNewsItem } from '../../../core/models/featured-news.models';
   styleUrl: './featured-news.css',
 })
 export class FeaturedNews {
+  private fullTitle = 'LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT';
+
+  private randomTitle(): string {
+    const words = this.fullTitle.split(' ');
+    const min = 3;
+    const max = words.length;
+    const count = Math.floor(Math.random() * (max - min + 1)) + min;
+    return words.slice(0, count).join(' ');
+  }
+
   items: FeaturedNewsItem[] = Array.from({ length: 11 }, (_, i) => ({
     image: `images/news${(i % 11) + 1}.png`,
     alt: 'Lorem Ipsum Dolor',
-    title: 'LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT',
+    title: this.randomTitle(),
     category: 'LOREM',
     date: '4.16.25',
   }));
