0ae7d8550c9801b556621bee1dbd3241a575ea2c
YalukR <yalukramos@gmail.com>
2026-05-29 02:15:19 -0600

    nueva sección

--- src/app/pages/home/featured-news/featured-news.ts
+++ src/app/pages/home/featured-news/featured-news.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FeaturedNewsItem } from '../../../core/models/featured-news.models';
 
 @Component({
   selector: 'app-featured-news',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './featured-news.html',
   styleUrl: './featured-news.css',
 })
-export class FeaturedNews {}
+export class FeaturedNews {
+  items: FeaturedNewsItem[] = Array.from({ length: 11 }, (_, i) => ({
+    image: `images/news${(i % 11) + 1}.png`,
+    alt: 'Lorem Ipsum Dolor',
+    title: 'LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT',
+    category: 'LOREM',
+    date: '4.16.25',
+  }));
+}
